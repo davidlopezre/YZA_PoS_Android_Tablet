c39b5d77bfb239744838719add70bf5110dd32a6
David Lopez <dlopez1@student.unimelb.edu.au>
2017-12-21 14:08:45 +1100

    Setup the project. Added resources, color and wireframed some of the layouts

---
+++ app/src/main/java/com/pos/yza/yzapos/ProductActivity.java
@@ -0,0 +1,13 @@
+package com.pos.yza.yzapos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
+}
---
+++ app/src/main/java/com/pos/yza/yzapos/ProductCategoryActivity.java
@@ -0,0 +1,13 @@
+package com.pos.yza.yzapos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProductCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_category);
+    }
+}
