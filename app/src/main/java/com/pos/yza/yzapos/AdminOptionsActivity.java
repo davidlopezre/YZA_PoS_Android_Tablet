2f696c6d6668fb36803ac1a49b304bccf89f0244
David Lopez <dlopez1@student.unimelb.edu.au>
2017-12-23 13:01:39 +1100

    Added navigation drawer to admin options

--- app/src/main/java/com/pos/yza/yzapos/AdminOptionsActivity.java
+++ app/src/main/java/com/pos/yza/yzapos/AdminOptionsActivity.java
@@ -1,13 +1,65 @@
 package com.pos.yza.yzapos;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class AdminOptionsActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_options);
+
+        // Set up the toolbar.
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        // Set up the navigation drawer.
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
     }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.product_navigation_menu_item:
+                                // Do nothing, we're already on that screen
+                                break;
+                            case R.id.category_navigation_menu_item:
+//                                Intent intent =
+//                                        new Intent(TasksActivity.this, StatisticsActivity.class);
+//                                startActivity(intent);
+                                break;
+                            case R.id.staff_navigation_menu_item:
+                                // Do nothing, we're already on that screen
+                                break;
+                            default:
+                                break;
+                        }
+                        // Close the navigation drawer when an item is selected.
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
 }
--- app/src/main/java/com/pos/yza/yzapos/manageproduct/ManageProductActivity.java
+++ app/src/main/java/com/pos/yza/yzapos/manageproduct/ManageProductActivity.java
@@ -3,6 +3,8 @@ package com.pos.yza.yzapos.manageproduct;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pos.yza.yzapos.R;
+
 public class ManageProductActivity extends AppCompatActivity {
 
     @Override
